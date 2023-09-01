06fe580c417a299c214af3a66fe7d06110891001
Albert Nafría <albert.nafria@gmail.com>
2023-09-01 06:49:23 +0200

    Git init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Droplet Setup
+
+This repo contains the files for getting a machine ready to go right after
+deploying a droplet.
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install vim-gtk \
+    git tmux python \
+    ffuf -y
+
+# Configure firewall
+
+# Download dotfiles
